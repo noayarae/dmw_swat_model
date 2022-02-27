0e417f2ea8eaa05fe11437000981977008c942c3
noayarae <enoay7@yahoo.com>
2022-02-27 15:17:13 -0800

    Add files via upload

---
+++ TxtInOut/000010016.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:16 Subbasin:1 HRU:16 Luse:FRSE Soil: 61481 Slope: 20-9999 8/26/2020 12:00:00 AM ArcSWAT 2012.10_4.19
+ Soil Name: Olyic
+ Soil Hydrologic Group: B
+ Maximum rooting depth(mm) : 1140.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : SIL-SICL-UWB
+ Depth                [mm]:      300.00     1040.00     1140.00
+ Bulk Density Moist [g/cc]:        1.15        1.25        0.00
+ Ave. AW Incl. Rock Frag  :        0.19        0.19        0.00
+ Ksat. (est.)      [mm/hr]:       32.40       32.40       27.72
+ Organic Carbon [weight %]:        3.77        1.74        0.00
+ Clay           [weight %]:       21.00       31.00        0.00
+ Silt           [weight %]:       52.70       49.00        0.00
+ Sand           [weight %]:       26.30       20.00        0.00
+ Rock Fragments   [vol. %]:        2.00        4.00        0.00
+ Soil Albedo (Moist)      :        0.23        0.23        0.23
+ Erosion K                :        0.32        0.37        0.00
+ Salinity (EC, Form 5)    :        0.00        0.00        0.00
+ Soil pH                  :        5.60        5.00        0.00
+ Soil CACO3               :        0.00        0.00        0.00
+                              
