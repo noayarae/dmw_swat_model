af033dda292d0a4190405e0385b47c8a4cfd3bd9
noayarae <enoay7@yahoo.com>
2022-02-27 12:22:47 -0800

    Add files via upload

---
+++ TxtInOut/000010003.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:3 Subbasin:1 HRU:3 Luse:FRST Soil: 61475 Slope: 5-20 8/26/2020 12:00:00 AM ArcSWAT 2012.10_4.19
+ Soil Name: Melby
+ Soil Hydrologic Group: B
+ Maximum rooting depth(mm) : 1440.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : SPM-SIL-SICL-SIC-WB
+ Depth                [mm]:       20.00      250.00      660.00     1190.00     1440.00
+ Bulk Density Moist [g/cc]:        0.20        1.20        1.25        1.40        0.00
+ Ave. AW Incl. Rock Frag  :        0.42        0.20        0.20        0.15        0.00
+ Ksat. (est.)      [mm/hr]:     1342.80       32.40       32.40       10.80        2.52
+ Organic Carbon [weight %]:       43.50        2.61        1.16        0.44        0.00
+ Clay           [weight %]:       15.00       22.50       37.50       42.50        0.00
+ Silt           [weight %]:       50.00       52.70       54.90       49.80        0.00
+ Sand           [weight %]:       50.00       24.80        7.60        7.70        0.00
+ Rock Fragments   [vol. %]:        0.00        2.00        2.00        2.00        0.00
+ Soil Albedo (Moist)      :        0.16        0.16        0.16        0.16        0.16
+ Erosion K                :        0.00        0.32        0.43        0.32        0.00
+ Salinity (EC, Form 5)    :        0.00        0.00        0.00        0.00        0.00
+ Soil pH                  :        4.50        5.00        4.80        5.00        0.00
+ Soil CACO3               :        0.00        0.00        0.00        0.00        0.00
+                              
