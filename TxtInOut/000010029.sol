f6c143561fc5cd0c8180de69bb6e73a64542f4b5
noayarae <enoay7@yahoo.com>
2022-02-27 15:19:33 -0800

    Add files via upload

---
+++ TxtInOut/000010029.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:29 Subbasin:1 HRU:29 Luse:RNGB Soil: 61500 Slope: 5-20 8/26/2020 12:00:00 AM ArcSWAT 2012.10_4.19
+ Soil Name: Tolke
+ Soil Hydrologic Group: B
+ Maximum rooting depth(mm) : 1550.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : SIL-SICL
+ Depth                [mm]:      250.00     1550.00
+ Bulk Density Moist [g/cc]:        0.80        0.78
+ Ave. AW Incl. Rock Frag  :        0.28        0.25
+ Ksat. (est.)      [mm/hr]:       32.40       32.40
+ Organic Carbon [weight %]:        4.35        2.03
+ Clay           [weight %]:       22.50       31.00
+ Silt           [weight %]:       68.00       62.10
+ Sand           [weight %]:        9.50        6.90
+ Rock Fragments   [vol. %]:        6.00        0.00
+ Soil Albedo (Moist)      :        0.23        0.23
+ Erosion K                :        0.24        0.32
+ Salinity (EC, Form 5)    :        0.00        0.00
+ Soil pH                  :        5.00        4.80
+ Soil CACO3               :        0.00        0.00
+                              
